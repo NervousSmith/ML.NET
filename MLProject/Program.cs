96103981e342ea73c05643a3b4c98e7a998f7142
NervousSmith <paw.kowalski@protonmail.com>
2020-03-24 09:25:43 +0100

    Initial commit

---
+++ MLProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
