d751d4beb640c00e0ec882cc53213ac4e09eae27
NervousSmith <48060180+NervousSmith@users.noreply.github.com>
2020-04-20 14:20:40 +0200

    Pierwsze wytrenowanie
    Dodana konwersja .rpt na .csv, odczytanie .csv i dodanie danych do modelu, wytrenowanie modelu dla pierwszego 1mln rekordów, pierwszy model

---
+++ MainProject/DatePrediction.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject
+{
+    class DatePrediction
+    {
+        [ColumnName("Score")]
+        public float arrival_date;
+        public DateTime ToDateTime()
+        {
+            var dateTime = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(arrival_date)).DateTime.ToLocalTime();
+            return dateTime;
+    }
+    }
+
+   
+}
